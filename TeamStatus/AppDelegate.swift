48f7e6bb1fc36214a1463d9eb47a8d43838c06ea
Marcin Religa <marcin@religa.net>
2017-06-21 23:14:54 +0100

    Empty lines

--- TeamStatus/AppDelegate.swift
+++ TeamStatus/AppDelegate.swift
@@ -17,7 +17,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	func applicationWillTerminate(_ aNotification: Notification) {
 		// Insert code here to tear down your application
 	}
-
-
 }
 
